f6d9beeaed710e0d9b5838b7c0b327f3356b6627
Kanishka Agarwal <145436431+KanishkaAgarwal29@users.noreply.github.com>
2024-10-09 23:35:31 +0530

    Add files via upload

---
+++ Day12-FLIPCARDS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+int t,n,x;
+scanf("%d",&t);
+for(int i=0;i<t;i++)
+{
+    scanf("%d%d",&n,&x);
+    if(x>n-x)
+    printf("%d\n",n-x);
+    else
+    printf("%d\n",x);
+}
+return 0;
+}
+
